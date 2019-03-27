0caec7973cd95e0124f9cd2776142252530c9b18
islam <fm_bouayache@esi.dz>
2019-03-27 01:20:32 +0100

    unit test: table-marque, table-utilisateurs componenet

--- src/app/components/table-marques/table-marques.component.spec.ts
+++ src/app/components/table-marques/table-marques.component.spec.ts
@@ -3,11 +3,18 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
 
 import { TableMarquesComponent } from './table-marques.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../../material';
+import {HttpClientModule} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {By} from '@angular/platform-browser';
 
 describe('TableMarquesComponent', () => {
   let component: TableMarquesComponent;
   let fixture: ComponentFixture<TableMarquesComponent>;
-
+  let mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TableMarquesComponent ],
@@ -16,7 +23,12 @@ describe('TableMarquesComponent', () => {
         MatPaginatorModule,
         MatSortModule,
         MatTableModule,
-      ]
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+      ],
+      providers: [{provide: Router, useValue: mockRouter}]
     }).compileComponents();
   }));
 
@@ -28,5 +40,25 @@ describe('TableMarquesComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+  it('should contains the search input', () => {
+    const element = fixture.debugElement.query(By.css('form'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains "Nouvelle marque" button', () => {
+    const element = fixture.debugElement.query(By.css('button'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains the table', () => {
+    const element = fixture.debugElement.query(By.css('table'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains the progess spinner', () => {
+    const element = fixture.debugElement.query(By.css('mat-progress-spinner'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains the table paginator', () => {
+    const element = fixture.debugElement.query(By.css('mat-paginator'));
+    expect(element.nativeElement).toBeTruthy();
   });
 });
--- src/app/components/table-utilisateurs-fabricants/table-utilisateurs-fabricants.component.spec.ts
+++ src/app/components/table-utilisateurs-fabricants/table-utilisateurs-fabricants.component.spec.ts
@@ -3,20 +3,33 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
 
 import { TableUtilisateursFabricantsComponent } from './table-utilisateurs-fabricants.component';
+import {TableMarquesComponent} from '../table-marques/table-marques.component';
+import {MaterialModule} from '../../material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {By} from '@angular/platform-browser';
 
 describe('TableUtilisateursFabricantsComponent', () => {
   let component: TableUtilisateursFabricantsComponent;
   let fixture: ComponentFixture<TableUtilisateursFabricantsComponent>;
-
+  let mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableUtilisateursFabricantsComponent ],
+      declarations: [ TableUtilisateursFabricantsComponent],
       imports: [
         NoopAnimationsModule,
         MatPaginatorModule,
         MatSortModule,
         MatTableModule,
-      ]
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+      ],
+      providers: [{provide: Router, useValue: mockRouter}]
     }).compileComponents();
   }));
 
@@ -28,5 +41,25 @@ describe('TableUtilisateursFabricantsComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+  it('should contains the search input', () => {
+    const element = fixture.debugElement.query(By.css('form'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains "Nouvelle marque" button', () => {
+    const element = fixture.debugElement.query(By.css('button'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains the table', () => {
+    const element = fixture.debugElement.query(By.css('table'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains the progess spinner', () => {
+    const element = fixture.debugElement.query(By.css('mat-progress-spinner'));
+    expect(element.nativeElement).toBeTruthy();
+  });
+  it('should contains the table paginator', () => {
+    const element = fixture.debugElement.query(By.css('mat-paginator'));
+    expect(element.nativeElement).toBeTruthy();
   });
 });
